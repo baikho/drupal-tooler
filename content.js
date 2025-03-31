6cd142d44d2e0c90bd3abe641ba9e0143dd16061
Sang Lostrie <sang.lostrie@gmail.com>
2025-04-01 01:01:55 +0200

    feat: highlight OP comments with badge on issue pages

--- content.js
+++ content.js
@@ -2,11 +2,13 @@
     const patchUrl = chrome.runtime.getURL('modules/composerPatchButton.js');
     const replyUrl = chrome.runtime.getURL('modules/commentReplyButton.js');
     const gitlabPatchUrl = chrome.runtime.getURL('modules/gitlabPatchFetcher.js');
+    const highlightOPCommentsPath = chrome.runtime.getURL('modules/highlightOPComments.js');
     const previewPath = chrome.runtime.getURL('modules/patchPreview.js');
 
     const patchModule = await import(patchUrl);
     const replyModule = await import(replyUrl);
     const gitlabModule = await import(gitlabPatchUrl);
+    import(highlightOPCommentsPath).then(mod => mod.highlightOPComments());
     import(previewPath).then(mod => mod.addInlinePatchPreviews());
 
     if (document.readyState === 'complete' || document.readyState === 'interactive') {
